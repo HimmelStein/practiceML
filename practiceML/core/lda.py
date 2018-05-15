9458c6185338bda128309b4d7fb89ee7503501b4
tiansi dong <tian1shi2@gmail.com>
2018-05-15 16:26:55 +0200

    add GridSearchCV function to select the best lda result

--- practiceML/core/lda.py
+++ practiceML/core/lda.py
@@ -64,10 +64,54 @@ def perform_lda_analysis(txtDir='', numOfTxts=None, numOfTopics=5, maxIter=20,
                                           n_jobs=nJobs)
 
     lda_result = lda_model.fit_transform(dataVector)
-    pprint(lda_result)
+    results = { 'result':lda_result,
+                'logLikelyhood': lda_model.score(dataVector), # the higher the better
+                'perplexity': lda_model.perplexity(dataVector), # the lower the better
+                'params': lda_model.get_params()
+                }
+    pprint(results)
+    return results
+
+
+def perform_best_lda_analysis(txtDir='', numOfTxts=None,
+                              numTopicsLst=[10,15,20,25,30], learningDecay=[.5,.7,.9]):
+    warnings.simplefilter("ignore", DeprecationWarning)
+    txtLst = []
+    for fname in os.listdir(txtDir)[:numOfTxts]:
+        with codecs.open(os.path.join(cfg.pwc['cleanTxtDir'], fname), 'r', 'utf-8-sig') as fh:
+            txt = get_content_words(fh.read())
+            txtLst.append(txt)
+    txtLst = txtLst
+    vectorizer = CountVectorizer(analyzer='word', min_df=4, lowercase=True,
+                                 token_pattern='[a-zA-Z0-9]{3,}')
+
+    dataVector = vectorizer.fit_transform(txtLst)
+    model = GridSearchCV(cv=None, error_score='raise',
+                         estimator=LatentDirichletAllocation(batch_size=128, doc_topic_prior=None,
+                                                             evaluate_every=-1, learning_decay=0.7, learning_method=None,
+                                                             learning_offset=10.0, max_doc_update_iter=100, max_iter=10,
+                                                             mean_change_tol=0.001, n_components=10, n_jobs=1,
+                                                             n_topics=None, perp_tol=0.1, random_state=None,
+                                                             topic_word_prior=None, total_samples=1000000.0, verbose=0),
+                         fit_params=None, iid=True, n_jobs=1,
+                         param_grid={'n_topics': numTopicsLst, 'learning_decay':learningDecay},
+                         pre_dispatch='2*n_jobs', refit=True, return_train_score='warn',
+                         scoring=None, verbose=0)
+    model.fit(dataVector)
+    bestLda = model.best_estimator_
+    result = {'bestLda': bestLda,
+              'params': model.best_params_,
+              'logLikelyhood': model.best_score_,
+              'perplexity': bestLda.perplexity(dataVector)}
+    pprint(result)
+    return result
 
 
 if __name__ == '__main__':
-    perform_lda_analysis(txtDir=cfg.pwc['cleanTxtDir'], numOfTxts=30, numOfTopics=5, maxIter=20,
-                         learningMode='online', randomState=100, batchSize=128,
-                         evaluateEvery=-1, nJobs=-1)
+    if False:
+        perform_lda_analysis(txtDir=cfg.pwc['cleanTxtDir'], numOfTxts=30, numOfTopics=5, maxIter=20,
+                             learningMode='online', randomState=100, batchSize=128,
+                             evaluateEvery=-1, nJobs=-1)
+    else:
+        perform_best_lda_analysis(txtDir=cfg.pwc['cleanTxtDir'], numOfTxts=30,
+                                  numTopicsLst=[10, 15, 20, 25, 30], learningDecay=[.5, .7, .9])
